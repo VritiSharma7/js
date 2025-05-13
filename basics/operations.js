19513ad569a6316a16b1a8a1359bf346785c9dc2
VritiSharma7 <vritisharma2005@gmail.com>
2025-05-13 13:18:32 +0000

    conversion

---
+++ basics/operations.js
@@ -0,0 +1,23 @@
+let val =3
+let negValue = -val
+console.log(negValue);
+
+console.log(2+2)
+console.log(2/2)
+console.log(2-2)
+console.log(2*2)
+console.log(2%2)
+
+let str1 = "vriti"
+let str2= "sharma"
+
+let str3= str1+ str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1 +"2");
+console.log("1" +2+2);
+console.log(1+2+"2");
+//what is difference bw 19 and 20th statement
+
+
