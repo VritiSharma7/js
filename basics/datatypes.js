70cec9f238204e7a34c20421aedf16b6f6b89e14
VritiSharma7 <vritisharma2005@gmail.com>
2025-05-13 12:52:14 +0000

    conversion of datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict";   //treat all JS code as newer version
+
+// alert(3+3) //we r using nodejs nt brows,alert cant be used
+
+console.log(3 + 3)
+
+console.log("vriti")
+
+let name = 'vriti'
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean=> true/false
+// null=> standalone value iske type hote h object
+// undefined =>
+// symbol =>unique
+
+
+//object
+console.log(typeof age );
+console.log(typeof undefined); //undefined
+console.log(typeof null);   //object
+
+ 
---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+const accountID = 232025
+let accountEmail ="vritisharma2005@gmail.com"
+var accountPassword = "12345"
+accountCity ="Mohali"
+let accountState;
+
+// accountID =23 //not allowed bcoz const value fix hojati no change possible 
+
+accountEmail ="vikar@.com"
+accountPassword=2343
+accountCity ="Bengaluru"
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var 
+beacuse of issue in block scope and functional scope
+*/
